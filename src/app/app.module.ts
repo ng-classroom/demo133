1fbc49fc86c82530d710988b3c1705d83f623fa5
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2018-10-28 09:34:31 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { View1Component } from './view1/view1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    View1Component
   ],
   imports: [
     BrowserModule,
